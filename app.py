5a571e7c113c43f6614995aad14b0900ed5d690f
Ashrafhuzairie <ashrafhuzairie@gmail.com>
2025-05-07 14:24:53 +0800

    Updated Something

--- app.py
+++ app.py
@@ -118,7 +118,5 @@ def chat():
 if __name__ == '__main__':
         app.run(host="0.0.0.0" , port = 8080 , debug = True)
 
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port=8080, debug=False, use_reloader=False)
 
         
